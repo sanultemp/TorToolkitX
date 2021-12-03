a3fbe3ed4016e389db0f4ba96f9fe8f911c4d19a
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-12-03 22:40:13 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -4,18 +4,18 @@ except:
 
     class ExecVars:
         # Set true if its VPS
-        IS_VPS = False
+        IS_VPS = True
 
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
+        API_HASH = "4d4023337b8cc46c306af69adb5fc21a"
+        API_ID = 7469109
+        BOT_TOKEN = "5078521431:AAHDz1_5lJYjugyXDCLdtSbeuEuUxxdMLcA"
         BASE_URL_OF_BOT = ""
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
+        ALD_USR = [2118362497, -1001516237874]
         OWNER_ID = 0
 
         # Google Drive Index Link should include the base dir also See readme for more info
@@ -38,7 +38,7 @@ except:
 
         # DB URI for access
         DATABASE_URL = (
-            "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+            "postgres://xiqfwupvokveje:237bc1d5fad9a3180a6fa351cf4c816bcb0ae883d7df6658d499aef59799d403@ec2-34-194-14-176.compute-1.amazonaws.com:5432/dfqe18923io1s2"
         )
 
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
